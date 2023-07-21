d09309ec6ba02ed57b3c88055af636fd196e5cbd
jimbs <balmorijimbo@yahoo.com>
2023-07-21 23:20:55 +0800

    update use of vuetify

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import './assets/main.css'
-import '@mdi/light-font/css/materialdesignicons-light.css';
-import '@mdi/font/css/materialdesignicons.css';
-
+import '@mdi/light-font/css/materialdesignicons-light.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createVuetify } from 'vuetify'
-import * as ExpansionPanel from 'vuetify/components/VExpansionPanel'
+import * as components from 'vuetify/components'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-
-
 
 const app = createApp(App)
 
@@ -17,7 +14,7 @@ app.use(createPinia())
 
 app.use(
   createVuetify({
-    components: { ExpansionPanel }
+    components
   })
 )
 
